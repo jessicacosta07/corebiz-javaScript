cf00cf1b372d1103d839f5f2fcc51debd891aec7
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-16 14:57:02 -0300

    validacao de dados javascript

--- js/script.js
+++ js/script.js
@@ -7,5 +7,11 @@ function exibir() {
     let comissao = parseFloat(quantidadeVendas * 0.15).toFixed(2)
     let salarioMensal = (parseFloat(salarioFixo) + parseFloat(comissao)).toFixed(2)
 
-    resultado.innerHTML += `<p>Olá ${nome}, sua comissão é ${comissao} reais e salário mensal de ${salarioMensal} reais</p>`
+    if (nome.length == 0 || salarioFixo.length == 0 || quantidadeVendas.length == 0) {
+        resultado.innerHTML = 'Por favor, preencha os campos!'
+    } else if (salarioFixo < 0 || quantidadeVendas < 0) {
+        resultado.innerHTML = 'Verifique os dados informados, informe valores maior que 0!'
+    } else {
+        resultado.innerHTML += `<p>Olá ${nome}, sua comissão é ${comissao} reais e salário mensal de ${salarioMensal} reais</p>`
+    }
 }
