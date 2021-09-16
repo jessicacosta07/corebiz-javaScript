f57626acdf85ecdbc1fa3d3b8db624352ba9e4a9
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-16 14:32:57 -0300

    calculo do salario javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-let nome = document.querySelector('input#txtNome')
-let salarioFixo = document.querySelector('input#txtSalario')
-let quantidadeVendas = document.querySelector('input#txtVendas')
+function exibir() {
+    let nome = document.querySelector('input#txtNome').value
+    let salarioFixo = document.querySelector('input#txtSalario').value
+    let quantidadeVendas = document.querySelector('input#txtVendas').value
+    let resultado = document.querySelector('div#resultado')
 
-function calcularComissao(comissao) {
-    let comissao = quantidadeVendas * 0.15
-}
+    let comissao = parseFloat(quantidadeVendas * 0.15).toFixed(2)
+    let salarioMensal = (parseFloat(salarioFixo) + parseFloat(comissao)).toFixed(2)
 
-function calcularSalarioMensal(salarioMensal) {
-    let salarioMensal = salarioFixo + comissao
+    resultado.innerHTML += `<p>Olá ${nome}, sua comissão é ${comissao} reais e salário mensal de ${salarioMensal} reais</p>`
 }
